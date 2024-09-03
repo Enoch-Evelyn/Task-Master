82fd8881559f32f38364884fc18d3edb6eb4049b
Knock <enoch10evelyn@gmail.com>
2024-09-02 23:43:09 -0400

    Was able to create a input form, submit button(submit arguement) and stopped the page from reloading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,21 @@
 
-//Id is selected prefaced with a "#" while classes are prefaced with "."
-//El used to identify this a Dom element 
-
-
-// var taskItemEl = document.createElement("li");
-
-// taskItemEl.textContent = "hello";
-
-
-// //selects what element is being targeted 
-// var tasksToDoEl = document.querySelector("#tasks-to-do");
-
-// // //appends the element of taskItemEl
-// tasksToDoEl.appendChild(taskItemEl);
-
-
-
-
-
-
-// var buttonEl = window.document.querySelector("#save-task");
-// console.log(buttonEl)
-
-
-// buttonEl.addEventListener("click", function() {
-//     alert("button clicked");
-//   });
-
-
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
+//selects div in html to target 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function() {
+//event listener that proceeds to add text content and insert it into a div when the button is clicked 
+formEl.addEventListener("submit", function() {
+
+//stops the page from reloading
+  event.preventDefault();
+
+  //creates an element 
   var listItemEl = document.createElement("li");
+  //give the element a class of task-item
   listItemEl.className = "task-item";
+  //give the element textContent of what is provided
   listItemEl.textContent = "This is a new task.";
+  //add(append) all of what's LisItemEL to the targeted div 
   tasksToDoEl.appendChild(listItemEl);
 });
+
